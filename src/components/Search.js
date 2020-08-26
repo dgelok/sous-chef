7bb7a3ab6e162b52266fc6b4d559f9e80f414e8d
Dan Gelok <dgelok@gmail.com>
2020-08-26 16:01:30 -0500

    cleanup

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import SearchInputs from './SearchInputs'
 import SearchResults from './SearchResults'
-
+import {Container} from 'react-bootstrap'
 
 const Search = () => {
   return (
     <>
-      <h3>Here's the search page</h3>
-      <SearchInputs/>
-      <SearchResults/>
+    <Container>
+        <h3>Here's the search page</h3>
+        <SearchInputs/>
+        <br />
+        <br />
+        <br />
+        <SearchResults/>
+        
+    </Container>
     </>
   )
 }
