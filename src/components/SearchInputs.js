216588594fb79ae6e2d5e3dbaa295cf9da91adc3
Dan Gelok <dgelok@gmail.com>
2020-08-26 15:57:37 -0500

    searchbar wired up to itself

--- src/components/SearchInputs.js
+++ src/components/SearchInputs.js
@@ -1,23 +1,69 @@
 import React from 'react'
-import {Form, FormControl, Dropdown, Col, InputGroup, Button} from 'react-bootstrap'
+import {Form, FormControl, Input, Dropdown, Col, InputGroup, Button} from 'react-bootstrap'
 
 
 const SearchInputs = () => {
 
-    let handleClick = () =>{
-        console.log("You clicked!")
+    let handleClick = (e) =>{
+        e.preventDefault();
+        console.log(e.target.cuisine.value)
+        console.log(e.target.type.value)
+        console.log(e.target.keywords.value)
     }
 
     return (
         <>
         <Form onSubmit={handleClick}>
-            <Form.Row className="align-items-center">
-                <Dropdown className="m-1">
+            <Form.Row className="align-items-center m-2">
+
+                <Form.Group className="m-1" controlId="cuisine">
+                    <Form.Label>Select Cuisine</Form.Label>
+                    <Form.Control as="select" size="sm" custom>
+                    <option>any</option>
+                    <option>Italian</option>
+                    <option>Mexican</option>
+                    <option>Japanese</option>
+                    <option>Vegetarian</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group className="m-1" controlId="type">
+                    <Form.Label>Select Type</Form.Label>
+                    <Form.Control as="select" size="sm" custom>
+                    <option>any</option>
+                    <option>Breakfast</option>
+                    <option>Soup</option>
+                    <option>Dessert</option>
+                    <option>Main Course</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group className="m-1" controlId="keywords">
+                    <Form.Label>Search Terms</Form.Label>
+                    <Form.Control size="sm" type="text" placeholder="any" />
+                </Form.Group>
+                
+                
+                    <Button type="submit" className="mt-4 ml-1">
+                        Submit
+                    </Button>
+                
+                {/* <Col xs="auto">
+                <label for="exampleFormControlSelect1">Select Type</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Breakfast</option>
+                    <option>Soup</option>
+                    <option>Dessert</option>
+                    <option>Beverage</option>
+                    <option>Main Course</option>
+                </select>
+                </Col> */}
+
+
+                {/* <Dropdown className="m-1">
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
                         Choose Cuisine Type
                     </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
+                    <Dropdown.Menu id="cuisine">
                         <Dropdown.Item href="#/action-1">Italian</Dropdown.Item>
                         <Dropdown.Item href="#/action-2">Greek</Dropdown.Item>
                         <Dropdown.Item href="#/action-3">Mexican</Dropdown.Item>
@@ -25,14 +71,14 @@ const SearchInputs = () => {
                         <Dropdown.Item href="#/action-5">Thai</Dropdown.Item>
                         <Dropdown.Item href="#/action-6">Other</Dropdown.Item>
                     </Dropdown.Menu>
-                </Dropdown>
+                </Dropdown> */}
                 
-                <Dropdown className="m-1">
+                {/* <Dropdown className="m-1">
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
                         Choose Menu Type
                     </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
+                    <Dropdown.Menu id="Type">
                         <Dropdown.Item href="#/action-1">Breakfast</Dropdown.Item>
                         <Dropdown.Item href="#/action-2">Soup</Dropdown.Item>
                         <Dropdown.Item href="#/action-3">Dessert</Dropdown.Item>
@@ -40,14 +86,8 @@ const SearchInputs = () => {
                         <Dropdown.Item href="#/action-5">Snack</Dropdown.Item>
                         <Dropdown.Item href="#/action-6">Main Course</Dropdown.Item>
                     </Dropdown.Menu>
-                </Dropdown>
-
-                <input className="m-1" type="text" id="keywords" ></input>
-                <Col xs="auto">
-                    <Button type="submit" className="m-1">
-                        Submit
-                    </Button>
-                </Col>
+                </Dropdown> */}
+                
             </Form.Row>
         </Form>
         </>
