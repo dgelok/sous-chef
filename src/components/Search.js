ea35217d906ab862f9b8fb6e3d6f55d00491dbad
Dan Gelok <dgelok@gmail.com>
2020-08-27 11:43:55 -0500

    refactored Search into class-based component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SearchInputs from './SearchInputs'
 import SearchResults from './SearchResults'
 import {Container} from 'react-bootstrap'
 
-const Search = () => {
-  return (
-    <>
-    <Container>
-        <h3 className="m-5">Here's the search page</h3>
-        <SearchInputs/>
-        <br />
-        <br />
-        <br />
-        <SearchResults/>
-        
-    </Container>
-    </>
-  )
+
+class Search extends Component {
+  render() {
+    return (
+      <>
+        <Container>
+            <h3 className="m-5">Here's the search page</h3>
+            <SearchInputs/>
+            <br />
+            <br />
+            <br />
+            <SearchResults/>
+            
+        </Container>
+      </>
+    )
+  }
 }
 
 export default Search
+
