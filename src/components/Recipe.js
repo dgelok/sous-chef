813606191889ccd881ec461b69e6da36a392e262
Dan Gelok <dgelok@gmail.com>
2020-08-28 14:23:50 -0500

    updating the recipe page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,19 +1,44 @@
 import React from 'react'
-import {Jumbotron, Button} from 'react-bootstrap'
+import {useSelector} from 'react-redux'
+import {Jumbotron, Button, Row, Col} from 'react-bootstrap'
 
-const Recipe = (props) => {
+const Recipe = () => {
+
+  const myRecipe = useSelector(state => state.individualRecipe)
+  
+  const renderHTMLContent = (htmlContent) =>
+  React.createElement('div', {
+    dangerouslySetInnerHTML: { __html: htmlContent},
+  });
+
+  let summary = renderHTMLContent(myRecipe.summary)
+
   return (
     <>
       
-      <Jumbotron>
-        <h1>Title</h1>
+      <Jumbotron className="">
+        <h1>{myRecipe.title}</h1>
+        <br />
+        <img src={myRecipe.image} />
         <p>
-          description
+          <Button className="m-2" variant="primary">Add to Shopping List</Button>
         </p>
-        <p>
-          <Button variant="primary">Add to Shopping List</Button>
-        </p>
+        {summary}
       </Jumbotron>
+
+      <Row>
+        <Col>
+          {myRecipe.instructions}
+        </Col>
+        <Col>
+        <ul>
+          <li>ingredient here</li>
+          <li>ingredient here</li>
+          <li>ingredient here</li>
+        </ul>
+        </Col>
+      </Row>
+
           </>
   )
 }
