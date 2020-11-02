5afedb560435a70b80d1219ccec184fa2e84f19f
Dan Gelok <dgelok@gmail.com>
2020-11-02 11:08:08 -0600

    cleanup

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,7 +32,7 @@ class Search extends Component {
       <div className='bgImage2'>
         <Container>
           <Row className="d-flex justify-content-center pt-3">
-            <h1 className="mb-5 display-3 pt-3">Search through 280,000+ recipes</h1>
+            <h1 className="mb-5 display-3 pt-3">Search through 380,000+ recipes</h1>
             <SearchInputs changeState={this.changeState}/>
           </Row>
             <br />
